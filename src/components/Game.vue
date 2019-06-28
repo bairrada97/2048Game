32d0d64ce03e5bf323ed6f2679c1b9fcbd9ec065
Unknown <bairrada1997@gmail.com>
2019-06-28 18:05:18 +0100

    upd: game logic

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -28,11 +28,29 @@ export default {
 
   },
   mounted() {
-
     this.renderBoard();
     this.addRandomNumber(2);
+    window.addEventListener('keypress', ({
+      keyCode
+    }) => {
+      if (keyCode === 32) {
+        this.slide();
+        this.sumNumbers();
+        this.slide();
+        this.addRandomNumber(1);
+      }
+
+    });
   },
   methods: {
+    isInBounds(target) {
+      if (target != undefined) {
+        return (target.x >= 0 && target.x <= this.board.length - 1) && (target.y >= 0 && target.y <= this.board.length - 1)
+      } else {
+        return false
+      }
+
+    },
     renderBoard() {
       for (let r = 0; r < 16; r++) {
         let x = Math.floor(r / this.rowSize),
@@ -45,19 +63,54 @@ export default {
         });
       }
     },
-    addRandomNumber(nr) {
+    addRandomNumber(number) {
 
-      for (let i = 0; i < nr; i++) {
-        let boardMap = this.board.filter(elm => elm.numbers == "" );
+      for (let i = 0; i < number; i++) {
+        let boardMap = this.board.filter(element => element.numbers == "");
         let randomTile = boardMap[Math.floor(Math.random() * boardMap.length)],
           randomNumber = this.numbers[Math.floor(Math.random() * this.numbers.length)];
+        randomTile.numbers = randomNumber;
+      }
 
-          randomTile.numbers = randomNumber;
+    },
+    slide(index) {
+      let hasNumbers = this.board.filter(element => element.numbers != "");
+      const reverse  = hasNumbers.map((val, index) => hasNumbers[hasNumbers.length - 1 - index]); // reverse array, element on for each will be the last element on Collumn;
+
+      for(let element of reverse){
+
+        const getEmpty = this.board.filter((item) => {
+          return item.x > element.x && item.y == element.y && item.numbers == "";  // get array of empty piece on Collumn
+        });
+
+        if(getEmpty.length > 0){
+          getEmpty[getEmpty.length - 1].numbers = element.numbers; //element to array index
+          element.numbers = ""
+        }else{
+
+          continue
+        }
 
       }
 
-
     },
+    sumNumbers(){
+      for (let element of this.board) {
+        const getPieceNumber = this.board.filter((item) => {
+          return item.x > element.x && item.y == element.y && item.numbers != "";  // get array of empty piece on Collumn
+        });
+
+        if(getPieceNumber.length > 0){
+          if(getPieceNumber[getPieceNumber.length - 1].numbers ==  element.numbers){
+            getPieceNumber[getPieceNumber.length - 1].numbers += element.numbers;
+            element.numbers = ""
+          }else{
+            continue
+          }
+        }
+      }
+
+    }
 
 
   },
